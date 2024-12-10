1e063a3eb6a5ff08a9c639271e46b4357b421684
GabrielReynaFranco <a01572655@tec.mx>
2024-12-09 18:04:47 -0600

    Chassis and Intake Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
@@ -1,4 +1,58 @@
 package org.firstinspires.ftc.teamcode;
+import org.firstinspires.ftc.teamcode.Commands.MoveIntake;
+import org.firstinspires.ftc.teamcode.Subsystems.Chassis;
+import org.firstinspires.ftc.teamcode.Commands.Drive;
+import org.firstinspires.ftc.teamcode.Subsystems.Intake;
 
-public class MainSystem {
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.command.button.Button;
+
+
+@TeleOp
+public class MainSystem extends LinearOpMode {
+    @Override
+        public void runOpMode(){
+            Chassis chassis = new Chassis(hardwareMap); //Here you can add every element of the robot
+            Intake intake = new Intake(hardwareMap);
+            GamepadEx driver = new GamepadEx(gamepad1);
+            GamepadEx operator = new GamepadEx(gamepad2);
+
+            /*------------------------------------------------------------------*/
+            /*------------------------------------------------------------------*/
+
+            chassis.setDefaultCommand(new Drive(chassis,gamepad1));
+
+            Button operatorButtonY= operator.getGamepadButton(GamepadKeys.Button.Y);
+            operatorButtonY.whenHeld(new MoveIntake(intake,1.0));
+            operatorButtonY.whenReleased(new MoveIntake(intake,0.0));
+
+            Button operatorButtonB= operator.getGamepadButton(GamepadKeys.Button.B);
+            operatorButtonB.whenHeld(new MoveIntake(intake,-1.0));
+            operatorButtonB.whenReleased(new MoveIntake(intake,0.0));
+
+            waitForStart();
+            chassis.reset(new Pose2d(0,0, Rotation2d.fromDegrees(0))); /*When the Op mode starts,
+                                                                every value will return to be zero*/
+
+            while (opModeIsActive()) { //This will occur whenever the op mode is active
+                CommandScheduler.getInstance().run();
+                Pose2d pose = chassis.getPose();
+
+                // -- ODOMETRY TELEMETRY -- //
+                telemetry.addData("X", pose.getX()); //This will display the telemetry on the DriverHub
+                telemetry.addData("Y", pose.getY());
+                telemetry.addData("Heading", pose.getRotation().getDegrees());
+                telemetry.addData("RightDistance", chassis.rightDistance());
+                telemetry.addData("LeftDistance", chassis.leftDistance());
+
+                // -- UPDATE TELEMETRY -- //
+                telemetry.update();
+            }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Chassis.java
@@ -1,4 +1,77 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.overture.ftc.overftclib.Devices.IOverDcMotor;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveOdometry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-public class Chassis {
+
+
+public class Chassis extends SubsystemBase {
+    private IOverDcMotor left_DriveTrain;
+    private IOverDcMotor right_DriveTrain;
+
+    private final double TICKS_PER_METER = 28.0;
+    private final double TRACK_WIDTH = 0.09;
+    private final double REDUCTION = 20.0;
+
+    private DifferentialDriveOdometry differentialDriveOdometry;
+    private IMU imu;
+    private double leftOffset = 0.0;
+    private double rightOffset = 0.0;
+
+    public Chassis (HardwareMap hardwareMap){
+        left_DriveTrain = (IOverDcMotor) hardwareMap.get(DcMotorEx.class,"left_Drive");
+        right_DriveTrain = (IOverDcMotor) hardwareMap.get(DcMotorEx.class, "right_Drive");
+
+        left_DriveTrain.setDirection(IOverDcMotor.Direction.FORWARD);
+        right_DriveTrain.setDirection(IOverDcMotor.Direction.REVERSE);
+
+        differentialDriveOdometry = new DifferentialDriveOdometry(new Rotation2d());
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters imuParemeters = new IMU.Parameters(new
+                RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(imuParemeters);
+        imu.resetYaw();
+    }
+
+    public void setVelocity( double linearVelocity, double angularVelocity){
+        left_DriveTrain.setPower(linearVelocity - angularVelocity);
+        right_DriveTrain.setPower(linearVelocity + angularVelocity);
+
+    }
+    public double leftDistance(){
+      return ((left_DriveTrain.getCurrentPosition() / TICKS_PER_METER) * TRACK_WIDTH * Math.PI) / REDUCTION;
+    }
+    public double rightDistance(){
+        return ((right_DriveTrain.getCurrentPosition() / TICKS_PER_METER) * TRACK_WIDTH * Math.PI) / REDUCTION;
+    }
+
+    public void reset(Pose2d pose2d){
+        leftOffset = left_DriveTrain.getCurrentPosition();
+        rightOffset = right_DriveTrain.getCurrentPosition();
+        differentialDriveOdometry.resetPosition(pose2d, getIMUHeading());
+    }
+
+    public Pose2d getPose(){
+        return differentialDriveOdometry.getPoseMeters();
+    }
+
+    @Override
+    public void periodic(){
+        differentialDriveOdometry.update(getIMUHeading(),leftDistance(),rightDistance());
+    }
+    private Rotation2d getIMUHeading(){
+        YawPitchRollAngles robotOrientation;
+        robotOrientation = imu.getRobotYawPitchRollAngles();
+        return Rotation2d.fromDegrees(robotOrientation.getYaw(AngleUnit.DEGREES));
+    }
+
 }
