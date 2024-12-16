2a44e16c96ceffca5098310c0bb09b73ddab0171
AlariiHP <a01572813@gmail.com>
2024-12-16 13:56:18 -0600

    Arm implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm.java
@@ -1,62 +1,81 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
-import com.overture.ftc.overftclib.Devices.IOverDcMotor;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.overture.ftc.overftclib.Contollers.ProfiledPIDController;
 import com.overture.ftc.overftclib.Contollers.TrapezoidProfile;
-import java.lang.Math;
 
-
-public class Arm extends SubsystemBase{
+public class Arm extends SubsystemBase {
     private final DcMotor armMotor;
     private final AnalogInput potentiometer;
+
+    private double lastTargetAngle;
+    private double lastMotorPower;
+
+    // PID and trapezoid profile constraints
     private final ProfiledPIDController profiledPIDController;
-    public Arm ( HardwareMap hardwareMap){
+
+    public Arm(HardwareMap hardwareMap) {
         armMotor = hardwareMap.get(DcMotor.class, "arm_Motor");
         potentiometer = hardwareMap.get(AnalogInput.class, "potentiometer");
+
         armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         armMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(5, 2.5);
-        profiledPIDController = new ProfiledPIDController(0.01,0.0,0.0, constraints);
+        // Profiled PID Controller
+        TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(200.0, 150.0);
+        profiledPIDController = new ProfiledPIDController(0.065, 0.0, 0.0, constraints);
 
-        //Tolerances
-        profiledPIDController.setTolerance(1.0);
+        // Tolerances
+        profiledPIDController.setTolerance(0.05);
         profiledPIDController.reset(voltageToAngle(potentiometer.getVoltage()));
-        profiledPIDController.enableContinuousInput(-180,180);
+        profiledPIDController.enableContinuousInput(-180, 180);
 
+
+    }
+
+    //Proportional feedforward to angle
+    private double feedForwardV(double neededAngle){
+        return Math.cos(neededAngle*(Math.PI/180))*(8/*feedforwarth value*/) ;
     }
     private double voltageToAngle(double voltage) {
-        //return ((-voltage + 3.3) * 62.15) - 45;
-        return (15.291 *Math.pow(this.getVoltage(), 2 )+27.952*this.getVoltage()+0.0907)-45;
+        return (15.291 * Math.pow(this.getVoltage(), 2) + 27.952 * this.getVoltage() + 0.0907) - 45;
     }
-    public void moveToPosition(double targetAngle){
-        double currentAngle= voltageToAngle(potentiometer.getVoltage());
 
+    public void moveToPosition(double targetAngle) {
+        double currentAngle = voltageToAngle(potentiometer.getVoltage());
         profiledPIDController.setGoal(targetAngle);
-        double power= profiledPIDController.calculate(currentAngle);
+        double power = profiledPIDController.calculate(currentAngle) + feedForwardV(currentAngle);
+        armMotor.setPower(Math.max(-1.0, Math.min(1.0, power)));
 
-        armMotor.setPower(power);
+        lastTargetAngle = targetAngle;
+        lastMotorPower = power;
     }
 
-    public boolean isAtTarget(){
+    public boolean isAtTarget() {
         return profiledPIDController.atGoal();
-
     }
 
     public double getVoltage(){
         return -potentiometer.getVoltage() + 3.3;
     }
 
-    public void stop(){
+    public void stop() {
         armMotor.setPower(0);
     }
 
-    public double getCurrentAngle(){
-        return voltageToAngle(this.getVoltage());
+    public double getTargetAngle() {
+        return lastTargetAngle;
+    }
+
+    public double getCurrentAngle() {
+        return voltageToAngle(potentiometer.getVoltage());
+    }
+
+    public double getMotorPower() {
+        return lastMotorPower;
     }
 }
